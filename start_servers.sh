5e16c6f37e3d482e8b5362661f6290d8f752ebce
Tired-man8 <tyavret@gmail.com>
2025-03-19 21:29:20 -0500

    Update start_servers.sh

--- start_servers.sh
+++ start_servers.sh
@@ -2,8 +2,8 @@
 
 # Start Flask in the background
 echo "Starting Flask server..."
-python app.py &
-
+# python app.py &
+gunicorn -w 4 -b 0.0.0.0:5000 app:app &
 # Start Go API
-echo "Starting Go API..."
+# echo "Starting Go API..."
 go run chinese_text_api.go
