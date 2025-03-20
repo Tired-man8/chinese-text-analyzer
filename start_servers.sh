2324d3572e38b13ff2e091d1ab0899eaaa023488
Tired-man8 <tyavret@gmail.com>
2025-03-19 23:52:10 -0500

    Update start_servers.sh

--- start_servers.sh
+++ start_servers.sh
@@ -7,16 +7,10 @@ export PATH=$HOME/.nix-profile/bin:$PATH  # Add Python to PATH
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements.txt
-
-echo "Installing Go..."
-curl -LO https://go.dev/dl/go1.20.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
 
 echo "Starting Flask server..."
-gunicorn -w 4 -b 0.0.0.0:5000 app:app &
+python app.py
 
 echo "Starting Go server..."
 go run chinese_text_api.go
-./chinese_text_api.go
+
