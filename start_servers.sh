9736dc9adb8319490acd29e7fd5df0b6a52c1254
Tired-man8 <tyavret@gmail.com>
2025-03-20 00:07:04 -0500

    Update start_servers.sh

--- start_servers.sh
+++ start_servers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "Installing Python dependencies..."
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "Starting Flask server..."
-python app.py
+python app.py &
 
 echo "Starting Go server..."
 go run chinese_text_api.go
